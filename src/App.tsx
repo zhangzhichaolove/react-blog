3063f499f0ea09d7ee0a9698e0b25bff4ec2f5e9
peakchao <zhangzhichaolove@vip.qq.com>
2021-02-02 22:45:11 +0800

    update route mode.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import { Switch, Route } from 'react-router-dom'
 import Home from './page/Home'
 import Class from './page/Class'
-import About from './page/About'
+// import About from './page/About'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/class' component={Class} />
-        <Route exact path='/about' component={About} />
+        <Route path='/' component={Home} />
+        <Route  path='/class' component={Class} />
+        {/* <Route exact path='/about' component={About} /> */}
       </Switch>
     </div>
   );
