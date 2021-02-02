1ab39171d0c8d9cd43d552c72e29e44de7cf3d1a
peakchao <zhangzhichaolove@vip.qq.com>
2021-02-02 17:23:04 +0800

    add react-router-dom.

---
+++ src/page/About/index.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class index extends Component {
+    render() {
+        return (
+            <div>
+                <h1>About</h1>
+            </div>
+        )
+    }
+}
