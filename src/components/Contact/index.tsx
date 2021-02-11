c7051bb67cc87fc248f7500865a62d41868388d6
peakchao <zhangzhichaolove@vip.qq.com>
2021-02-11 15:50:10 +0800

    update footer style.

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Divider } from 'antd'
-import { EnvironmentOutlined, WhatsAppOutlined, DisconnectOutlined } from '@ant-design/icons';
+import { EnvironmentOutlined, WhatsAppOutlined, MailOutlined } from '@ant-design/icons';
 import './index.css'
 
 export default function index() {
@@ -9,7 +9,7 @@ export default function index() {
             <Divider>联系方式</Divider>
             <div><EnvironmentOutlined className='iconStyle' />四川省 成都市</div>
             <div><WhatsAppOutlined className='iconStyle' />13594347817</div>
-            <div><DisconnectOutlined className='iconStyle' />admin@peakchao.com</div>
+            <div><MailOutlined className='iconStyle' />admin@peakchao.com</div>
         </div>
     )
 }
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { CopyrightOutlined } from '@ant-design/icons';
 import './index.css'
 
 export default class index extends Component {
@@ -6,6 +7,8 @@ export default class index extends Component {
         return (
             <div className='footerStyle'>
                 本博客由react+go强力驱动
+                <br />
+                <span><CopyrightOutlined />版权所有 蜀ICP备18012666号</span>
             </div>
         )
     }
