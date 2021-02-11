adc15079e1890dc39f6c6d87c48a3563e5bfcd74
peakchao <zhangzhichaolove@vip.qq.com>
2021-02-11 16:38:46 +0800

    add blog nav components.

---
+++ src/components/BlogNav/index.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { Divider } from 'antd'
+
+export default class index extends Component {
+    render() {
+        return (
+            <div>
+                <Divider>文章目录</Divider>
+                <div>11111</div>
+            </div>
+        )
+    }
+}
