92f8513d933c80bd178d67e9c10c956236f444cf
peakchao <zhangzhichaolove@vip.qq.com>
2021-02-13 20:12:15 +0800

    add author components link address.

--- src/components/Author/index.tsx
+++ src/components/Author/index.tsx
@@ -7,16 +7,15 @@ export default class index extends Component {
     render() {
         return (
             <div>
-                <Avatar size={80} src='https://secure.gravatar.com/avatar/ab0b8d7cc00b30d36d8cb1f67e86f551?s=192&d=mm&r=g' className='avatarStyle'/>
+                <Avatar size={80} src='https://secure.gravatar.com/avatar/ab0b8d7cc00b30d36d8cb1f67e86f551?s=192&d=mm&r=g' className='avatarStyle' />
                 <div>
                     独特编程思想才是真正的巨人、
                     <Divider>社交账号</Divider>
-                    <WechatOutlined className='socialStyle' />
-                    <WeiboCircleOutlined className='socialStyle' />
-                    <QqOutlined className='socialStyle' />
-                    <GithubOutlined className='socialStyle' />
+                    <a href="https://github.com/zhangzhichaolove"><WechatOutlined className='socialStyle' /></a>
+                    <a href="https://github.com/zhangzhichaolove"><WeiboCircleOutlined className='socialStyle' /></a>
+                    <a href="https://github.com/zhangzhichaolove"><QqOutlined className='socialStyle' /></a>
+                    <a href="https://github.com/zhangzhichaolove"><GithubOutlined className='socialStyle' /></a>
                 </div>
-
             </div>
         )
     }
