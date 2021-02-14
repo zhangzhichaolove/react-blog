173c8f4da8a18a299abbab9039da3072ebb49e9e
peakchao <zhangzhichaolove@vip.qq.com>
2021-02-14 22:45:24 +0800

    update 404 page.

--- src/page/NotFound/index.tsx
+++ src/page/NotFound/index.tsx
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
+import { Result, Button } from 'antd';
+import { Link } from 'react-router-dom'
 
 export default class index extends Component {
     render() {
         return (
-            <div>
-                404
-            </div>
+            <Result
+                status="404"
+                title="404"
+                subTitle="Sorry, 页面找不到啦."
+                extra={<Button type="primary"><Link to='/'>回到首页</Link></Button>}
+            />
         )
     }
 }
