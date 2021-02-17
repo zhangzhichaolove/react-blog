16b775c8a22fe89e790cc4960deb9214bdf9d3f5
peakchao <zhangzhichaolove@vip.qq.com>
2021-02-17 14:57:19 +0800

    update api address to auto.

--- src/util/AxiosUtil.ts
+++ src/util/AxiosUtil.ts
@@ -3,7 +3,7 @@ import { message as Message } from 'antd';
 import TokenUtil from "./TokenUtil";
 
 
-axios.defaults.baseURL = "http://localhost"
+axios.defaults.baseURL = "http://localhost"//window.location.host
 axios.defaults.headers = { token: TokenUtil.getToken() }
 axios.interceptors.request.use(config => {
     const token = TokenUtil.getToken()
