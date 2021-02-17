c441fd77a86c2fa9a692b701b3fcf2bcda9829ee
peakchao <zhangzhichaolove@vip.qq.com>
2021-02-17 13:52:39 +0800

    update label component api.

--- src/components/Label/index.tsx
+++ src/components/Label/index.tsx
@@ -1,23 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Divider, Tag } from 'antd'
 import { NavLink as LinkNav } from 'react-router-dom'
+import axios from 'axios'
+
 import './index.css'
 
-export default function index() {
+export default function Label() {
+    const [tags, setTags] = useState(new Array())
+    useEffect(() => {
+        axios('/api/findAllTag').then((res) => {
+            const labels = res.data.result
+            setTags(labels)
+        })
+    }, [])
     return (
         <div>
             <Divider>标签云</Divider>
             <div className='tagContainer'>
-                <LinkNav to={{ pathname: '/blogs', state: { tag: 'android' } }}><Tag color="magenta" className='tagStyle'>android</Tag></LinkNav>
-                <LinkNav to={{ pathname: '/blogs', state: { tag: 'java' } }}><Tag color="magenta" className='tagStyle'>java</Tag></LinkNav>
-                <LinkNav to={{ pathname: '/blogs', state: { tag: 'go' } }}><Tag color="magenta" className='tagStyle'>go</Tag></LinkNav>
-                <LinkNav to={{ pathname: '/blogs', state: { tag: 'python' } }}><Tag color="magenta" className='tagStyle'>python</Tag></LinkNav>
-                <LinkNav to={{ pathname: '/blogs', state: { tag: 'js' } }}><Tag color="magenta" className='tagStyle'>js</Tag></LinkNav>
-                <LinkNav to={{ pathname: '/blogs', state: { tag: '换肤' } }}><Tag color="magenta" className='tagStyle'>换肤</Tag></LinkNav>
-                <LinkNav to={{ pathname: '/blogs', state: { tag: '设计' } }}><Tag color="magenta" className='tagStyle'>设计</Tag></LinkNav>
-                <LinkNav to={{ pathname: '/blogs', state: { tag: 'linux' } }}><Tag color="magenta" className='tagStyle'>linux</Tag></LinkNav>
-                <LinkNav to={{ pathname: '/blogs', state: { tag: 'shell' } }}><Tag color="magenta" className='tagStyle'>shell</Tag></LinkNav>
-                <LinkNav to={{ pathname: '/blogs', state: { tag: '博客' } }}><Tag color="magenta" className='tagStyle'>博客</Tag></LinkNav>
+                {tags.map(item => <LinkNav to={{ pathname: '/blogs', state: { tag: item.name } }}><Tag color="magenta" className='tagStyle'>{item.name}</Tag></LinkNav>)}
             </div>
         </div>
     )
